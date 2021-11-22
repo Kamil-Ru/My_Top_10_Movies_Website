5a667e03d683be551f180a18639025eb7d65c9c6
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-11-23 00:13:17 +0100

    Working.

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ Bootstrap(app)
 
 db = SQLAlchemy(app)
 
+
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=False, nullable=False)
@@ -27,18 +28,20 @@ class Movie(db.Model):
     review = db.Column(db.Text, unique=True, nullable=True)
     img_url = db.Column(db.Text, nullable=False)
 
+
 class RateMovieForm(FlaskForm):
     rating = FloatField(label='New rating: ', validators=[DataRequired()])
     review = StringField(label="New review: ", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
 
+
 class AddMovieForm(FlaskForm):
     title = StringField(label="Title: ", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
 
+
 if not os.path.isfile("books-collection.db"):
     db.create_all()
-
 
 TMDB_Endpoint = "https://api.themoviedb.org/3/search/movie"
 
@@ -62,14 +65,16 @@ new_movie = Movie(
 db.session.add(new_movie)
 db.session.commit()
 """
+
+
 @app.route("/")
 def home():
     all_films = Movie.query.all()
     return render_template("index.html", all_films=all_films)
 
+
 @app.route("/edit", methods=["GET", "POST"])
 def edit():
-
     movie_id = request.args.get('id')
     movie_to_update = Movie.query.get(movie_id)
 
@@ -87,38 +92,38 @@ def edit():
 
     return render_template("edit.html", form=form)
 
+
 @app.route("/add", methods=["GET", "POST"])
 def add():
     form = AddMovieForm()
 
     if request.method == "POST" and form.validate_on_submit():
-
         parameters["query"] = form.title.data
-        return redirect("/select")
+        response = requests.get(TMDB_Endpoint, params=parameters)
+        response.raise_for_status()
+        data = response.json()
+        global list_of_move
+        list_of_move = []
+        for move in data['results']:
+            list_of_move.append(move)
+            #print(move)
+
+        return render_template("select.html", list_of_move=list_of_move)
 
     return render_template("add.html", form=form)
 
+
 @app.route("/select", methods=["GET", "POST"])
 def select():
+    print("OK")
+    move_id = int(request.args.get('move_id'))
 
-    if move_is_selected == "POST":
-        data = request.json()
-        pprint(data)
-        return redirect("/")
-
-    response = requests.get(TMDB_Endpoint, params=parameters)
-    response.raise_for_status()
-
-    data = response.json()
-
-    list_of_move = []
-    for move in data['results']:
-        list_of_move.append(move)
-
-
-    return render_template("select.html", list_of_move=list_of_move)
-
-
+    for move in list_of_move:
+        if move['id'] == move_id:
+            new_move = Movie(id=move['id'], title=move['title'], year=move['release_date'], description=move['overview'], ranking=move['vote_average'], img_url=f"https://image.tmdb.org/t/p/w500/{move['poster_path']}")
+            db.session.add(new_move)
+            db.session.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
